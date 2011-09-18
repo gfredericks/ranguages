ce645bf7ce904b598755b3c8d6a1c3aec22bb70d
Gary Fredericks <fredericksgary@gmail.com>
2011-09-17 20:41:01 -0400

    Getting fleshy

--- src/ranguages/core.clj
+++ src/ranguages/core.clj
@@ -1,7 +1,10 @@
 (ns ranguages.core
   (:require [ranguages.regex-parser :as rrp])
   (:refer-clojure :exclude [contains? reverse])
-  (:require [clojure.walk :as wk]))
+  (:require [clojure.walk :as wk]
+            [clojure.set :as sets]))
+
+(def epsilon ::epsilon)
 
 (defprotocol IRanguage
   (contains?     [r s] "returns true if the language contains the string/seq")
@@ -20,10 +23,26 @@
   (difference    [r1 r2]
     "Returns the language of all strings in r1 but not r2."))
 
+(defprotocol IHasStateNames
+  (prefix-state-names [r prefix]))
+
 (defn doh!
   []
   (throw (new java.lang.UnsupportedOperationException)))
 
+; Types:
+;   states: set of keywords
+;   alphabet: set of anything? (or chars...)
+;   transition:
+;     map from
+;       state-names
+;     to
+;       map from
+;         subsets of the alphabet (such that the keys form a partition of the alphabet)
+;       to
+;         a state name
+;   start: a state name
+;   accept: a set of state names
 (defrecord DFA [states alphabet transition start accept]
   IRanguage
   (to-dfa [d] d)
@@ -36,6 +55,19 @@
   (concatenation [d1 d2] (doh!))
   (difference [d1 d2] (doh!)))
 
+; Types:
+;   states: set of keywords
+;   alphabet: set of anything? (or chars...)
+;   transition:
+;     map from
+;       state-names
+;     to
+;       map from
+;         subsets of the (alphabet + ::epsilon) (such that the keys form a partition of the alphabet)
+;       to
+;         sets of state names
+;   start: a state name
+;   accept: a set of state names
 (defrecord NFA [states alphabet transition start accept]
   IRanguage
   (to-dfa [n] (doh!))
@@ -46,7 +78,27 @@
   (union [n1 n2] (doh!))
   (intersection [n1 n2] (doh!))
   (concatenation [n1 n2] (doh!))
-  (difference [n1 n2] (doh!)))
+  (difference [n1 n2] (doh!))
+  IHasStateNames
+  (prefix-state-names [n prefix]
+    (let [state-map (zipmap states (for [state states] (keyword (str prefix (name state)))))]
+      (new NFA
+           (-> state-map vals set)
+           alphabet
+           (zipmap
+             (map state-map states)
+             (for [state states]
+               (let [inner-transition-map (transition state)]
+                 (zipmap (keys inner-transition-map)
+                         (map #(set (map state-map %)) (vals inner-transition-map))))))
+           (state-map start)
+           (set (map state-map accept))))))
+
+(defn inject-state-machine
+  "Replaces the state outer-state in the NFA outer-machine
+  with the NFA inner-machine."
+  [outer-machine inner-machine outer-state]
+  )
 
 ; Second argument should be one of the following:
 ;   - a set of literals
@@ -59,7 +111,21 @@
   IRanguage
   (to-dfa [r] (doh!))
   (to-re [r] r)
-  (to-nfa [r] (doh!))
+  (to-nfa [r]
+    (cond
+      (set? regex-parse-tree)
+        (let [alphabet (conj alphabet ::epsilon)]
+          (new NFA
+               #{:a :b :c}
+               alphabet
+               {:a {regex-parse-tree #{:b},
+                    (sets/difference alphabet regex-parse-tree) #{}},
+                :b {alphabet #{}}},
+               :a
+               #{:b}))
+      (= :star (first regex-parse-tree))
+        (let [child-range (to-nfa (first regex-parse-tree))]
+          (doh!))))
   (reverse [r] (doh!))
   (star [r] (doh!))
   (union [r1 r2] (doh!))
@@ -70,7 +136,12 @@
 (defn parse-regex
   [alphabet re]
   (wk/postwalk
-    (fn [x] (new Regex alphabet x))
+    (fn [x]
+      (new Regex
+           alphabet
+           (if (set? x)
+             x
+             (cons (first x) (map parse-regex (rest x))))))
     (rrp/parse-regex alphabet (str re))))
 
 (defn optimize-dfa
--- test/ranguages/test/core.clj
+++ test/ranguages/test/core.clj
@@ -1,3 +1,19 @@
 (ns ranguages.test.core
   (:require [ranguages.core :as rc])
+  (:import ranguages.core.NFA)
   (:use [clojure.test]))
+
+(deftest prefix-state-names-test
+  (let [alpha (set "abc"),
+        alpha* (conj alpha rc/epsilon),
+        nfa (new NFA
+                 #{:foo :bar}
+                 alpha
+                 {:foo {alpha* #{:bar}},
+                  :bar {alpha* #{:bar}}}
+                 :bar
+                 #{:foo :bar}),
+        nfa* (rc/prefix-state-names nfa "xx")]
+    (is (= #{:xxfoo :xxbar} (:states nfa*)))
+    (is (= #{:xxbar}) (-> nfa* :transition :xxbar alpha*))
+    (is (= (:states nfa*) (-> nfa* :transition keys set)))))
