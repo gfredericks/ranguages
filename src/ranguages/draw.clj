b28277499ba84500530c04d6411b711a533ad204
Gary Fredericks <fredericksgary@gmail.com>
2011-09-21 13:57:37 -0500

    First draft of graphviz drawing

---
+++ src/ranguages/draw.clj
@@ -0,0 +1,52 @@
+(ns ranguages.draw
+  (:require [clojure.string :as string])
+  (:require [ranguages.core :as rc]))
+
+(defn- draw-state-machine
+  [states transitions]
+  (format
+    "digraph G{
+      overlap=scale;
+      splines=true;
+      %s
+      %s
+    }"
+    (string/join
+      "\n"
+      (for [{:keys [name label shape bgcolor]} states]
+        (format "%s [label=%s,shape=%s,style=filled,fillcolor=\"%s\"];"
+                name
+                label
+                shape
+                bgcolor)))
+    (string/join
+      "\n"
+      (for [{:keys [from to label]} transitions]
+        (format "%s -> %s [label=%s];"
+                (name from)
+                (name to)
+                label)))))
+
+(defn draw-dfa
+  [dfa]
+  (let [transitions
+          (mapcat
+            (fn [state]
+              (for [[chars state*] (-> dfa :transition state)]
+                {:from  state,
+                 :to    state*,
+                 :label (apply str chars)}))
+            (:states dfa)),
+        states
+          (for [state (:states dfa)]
+            {:name (name state),
+             :label (name state),
+             :shape
+               (if ((:accept dfa) state)
+                 "doublecircle"
+                 "circle"),
+             :bgcolor
+               (if (= state (:start dfa))
+                 "#6666FF"
+                 "white")})]
+    (draw-state-machine states transitions)))
